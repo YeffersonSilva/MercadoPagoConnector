b55d8f37e7d03e61ddb3f1f7c29f535b0241d605
Yefferson Silva <yeffersonsilvagomez@gmail.com>
2024-05-01 19:04:24 -0300

    refactor: improve folder structure

--- src/index.js
+++ src/index.js
@@ -1,7 +1,15 @@
 import express from 'express'
+import morgan from 'morgan'
+
+import paymentRoutes from './routes/payment.routes.js'
+import  {PORT} from './config.js'
 
 const app = express()
+app.use(morgan('dev'))
 
-app.listen(3000, () => {
+app.use( paymentRoutes)
+// /api/payment
+
+app.listen(PORT, () => {
     console.log('Server is running on http://localhost:3000')
-    }
+    })
--- src/routes/payment.routes.js
+++ src/routes/payment.routes.js
@@ -4,6 +4,13 @@ const router = Router();
 
 router.get("/creat-order", (req, res) => {
     res.send("Create order");
-}
+})
 
+router.get("/success", (req, res) => {
+    res.send("Success");
+})
+
+router.get("/webhook", (req, res) => {
+    res.send("Webhook");
+})
 export default router;
